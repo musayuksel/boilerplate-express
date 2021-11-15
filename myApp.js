b254e153324b82cd6bb1c3a792684b80eca32d11
musayuksel <musayuxel@gmail.com>
2021-11-15 10:39:54 +0000

    Chain Middleware to Create a Time Server

--- myApp.js
+++ myApp.js
@@ -5,13 +5,24 @@ require("dotenv").config();
 // app.get("/", (request, response) => {
 //   response.send("Hello Express");
 // });
-
+// Implement a Root-Level Request Logger Middleware
 app.use((req, res, next) => {
   const string =
     req.method + " " + req.path + " - " + req.ip;
   console.log(string);
   next();
 });
+// Chain Middleware to Create a Time Server
+app.get(
+  "/now",
+  (request, response, next) => {
+    request.time = new Date().toString();
+    next();
+  },
+  (request, response) => {
+    response.send({ time: request.time });
+  }
+);
 // Serve Static Assets
 const absolutePath = __dirname + "/public";
 app.use("/public", express.static(absolutePath));
