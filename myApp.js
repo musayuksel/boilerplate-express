08f50464b25f2193f9416c4f73f937f256f385c8
musayuksel <musayuxel@gmail.com>
2021-11-15 09:29:16 +0000

    Add Serve Static Assets ans Send File

--- myApp.js
+++ myApp.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
+// Hello Express
+// app.get("/", (request, response) => {
+//   response.send("Hello Express");
+// });
+
+// Serve Static Assets
+const absolutePath = __dirname + "/public";
+app.use("/public", express.static(absolutePath));
+// Send File
 app.get("/", (request, response) => {
-  response.send("Hello Express");
+  const absolutePath = __dirname + "/views/index.html";
+  response.sendFile(absolutePath, (err) => {
+    console.log("Error handler, >" + absolutePath);
+  });
 });
-console.log("Hello World");
+// console.log("Hello World");
 
 module.exports = app;
